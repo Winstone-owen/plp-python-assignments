412b00b77b9d65faa6b022ed9b8b3478d9df7938
Winstone-owen <maywakaowen@gmail.com>
2025-03-29 11:42:55 +0300

    Discounts

---
+++ week 3.py
@@ -0,0 +1,14 @@
+def  calculate_discount(price, discount_percent):
+
+    if discount_percent >= 20:
+        discount = price * (discount_percent / 100)
+        final_price = price - discount
+        return final_price
+    else:
+        return price
+
+price= float(input("Enter the price of the item: "))
+discount_percent= float(input("Enter the discount percentage: "))
+
+final_amount= calculate_discount(price, discount_percent)
+print(f"The final amount after discount is: {final_amount}")
