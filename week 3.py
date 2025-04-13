ceeb1c7b2bf3ef722867e52794f30472abfa976a
Winstone-owen <maywakaowen@gmail.com>
2025-04-13 19:43:25 +0300

    week 4

--- week 3.py
+++ week 3.py
@@ -11,4 +11,4 @@ price= float(input("Enter the price of the item: "))
 discount_percent= float(input("Enter the discount percentage: "))
 
 final_amount= calculate_discount(price, discount_percent)
-print(f"The final amount after discount is: {final_amount}")
+print(f"The total amount after discount is: {final_amount}")
