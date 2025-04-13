f835f9c74d09572ff4ac8cfc320bd831c266306b
Winstone-owen <maywakaowen@gmail.com>
2025-04-13 20:53:32 +0300

    file handling

--- week 4.py
+++ week 4.py
@@ -1,1 +1,17 @@
- status
+def read_and_mofify_file():
+    input_file_name = input("Enter the name of the file you want to read: ")
+
+    try:
+        with open(input_file_name, 'r') as file:
+            data=file.read()
+    except FileNotFoundError:
+        print(f"File {input_file_name} not found.")
+        return
+    
+
+#writing the modified version to a new file
+    output_file_name = input("Enter the name of the file you want to write: ")
+    with open(output_file_name, 'w') as file:
+        # reversing order of characters in the file
+        file.write(data[::-1])
+        print(f"File {output_file_name} has been created with modified content.")
